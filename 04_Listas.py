071c3c21608e0a06993da7f3409b21453072a363
Santiago Arias Aguirre <aguirresanti51@gmail.com>
2023-03-08 19:32:14 -0500

    Clase de listas terminada

--- 04_Listas.py
+++ 04_Listas.py
@@ -1,7 +1,7 @@
 ### Listas ###
 
 '''
-Una lista es una forma de estructura de datos, como su nombre lo indica esto nos permitira crear listas de datos de cualquier tipo y con ella realizar varias operaciones, desde las mas sencillas, hasta las mas complejas. Se podria tomar una lista como un "Array" o "Arreglo", ya que guardan cierto parentesco, no son lo mismo porque la lista trae operaciones un poco mas complejas. Sin embargo una lista acaba teniendo arrays pero la lista trae operadores como "Reverse", "Ordenaciones", Entre otras.
+Una lista es una forma de estructura de datos, como su nombre lo indica esta nos permitira crear listas de datos de cualquier tipo y con ella realizar varias operaciones, desde las mas sencillas, hasta las mas complejas. Se podria tomar una lista como un "Array" o "Arreglo", ya que guardan cierto parentezco, no son lo mismo porque la lista trae operaciones un poco mas complejas. Sin embargo una lista acaba teniendo arrays pero la lista trae operadores como "Reverse", "Ordenaciones", Entre otras.
 '''
 
 #Definir y Crear una lista:
@@ -10,7 +10,9 @@ Se llama la funcion "list()" para indicar que lo que queremos guardar en la vari
 Se puede guardar cualquier tipo de informacion en una lista, no hace falta seguir un mismo tipo de dato, una lista puede tener valores: numericos, de texto, decimales, entre otros.
 '''
 
-mi_lista = list(['Maria Fernanda', 18, 'Juanes', 20]) 
+# Maneras de crear una lista
+
+mi_lista = list(['Maria Fernanda', 18, 'Juanes', 20]) # Constructor
 mi_lista2 = ['Perro', 'Gato', 'Pajaro']
 
 print(mi_lista)
@@ -24,15 +26,15 @@ print('-------------------------------------------------------------------------
 mi_lista = ['Santy', 17, 1.75, 'Valeria', 17, 1.65, 'Santiago Arias', 21, 1.80, True, False]
 
 print(mi_lista)
-print(len(mi_lista))
-print(type(mi_lista))
+print('Cuantos valores tiene nuestra lista:', len(mi_lista))
+print('Que tipo de datos es: ', type(mi_lista))
 
 print('----------------------------------------------------------------------------------------------------------')
 
 #Acceder a la informacion de la lista
 
 print('El nombre de la primera persona en la lista es: ', mi_lista[0])
-print(mi_lista[-10])
+print('Que numero hay en esta posicion: ', mi_lista[-10])
 print('¿Cuantas personas hay con la misma edad?: ', mi_lista.count(17)) # Imprimame cuantas veces hay repetido un valor.
 
 #Que no se debe de hacer para acceder a toda la lista
@@ -43,4 +45,69 @@ print('-------------------------------------------------------------------------
 
 #Concatenar listas
 
-print(mi_lista + mi_lista2)
+print('Resultado despues de concatenar listas: ', mi_lista + mi_lista2)
+
+print('----------------------------------------------------------------------------------------------------------')
+
+
+# Como agregar nuevos valores a nuestra lista
+
+mi_lista.append(17) # Agrega informacion nueva a nuestra lista
+print('Agregar un nuevo resultado a la lista: ', mi_lista)
+
+mi_lista2.insert(0, 'Leon') # Insertar informacion en cierta posicion
+print(mi_lista2)
+
+# Eliminar elementos de una lista
+
+mi_lista.remove(17) # Remover el valor de la lista madre
+print('La lista queda asi despues de remover el valor 17: ', mi_lista)
+
+milista3 = mi_lista2.pop(0)
+print(milista3)
+print(mi_lista2)
+
+
+# Copiar una lista
+
+milista4 = [12, 120, 100, 400, 1, 10, 0]
+mi_nueva_lista = mi_lista.copy()
+
+mi_lista.clear() # Vaciar una lista
+print(mi_lista)
+
+print(mi_nueva_lista)
+
+
+print(milista4)
+print(milista4[0:2])
+
+
+milista4.sort() # Organizar los valores de una lista de menor a mayor
+print(milista4)
+
+milista4.reverse() # Organizar la lista de mayor a menor
+print(milista4)
+
+print('----------------------------------------------------------------------------------------------------------')
+
+# Corregir un elemento en una lista
+
+mi_lista2[0] = 'Daniel'
+print(mi_lista2)
+
+mi_lista2.sort()
+print(mi_lista2)
+
+mi_lista2.reverse()
+print(mi_lista2)
+
+'''
+Nota: No se pueden organizar listas que contengan numeros y textos
+'''
+
+print('----------------------------------------------------------------------------------------------------------')
+
+# Sublistas
+
+print(milista4[0:2])
---
+++ Ejercicios/Funcion-Lineal.py
@@ -0,0 +1,10 @@
+def funcion_lineal(x): # Tome como argumento un valor x
+
+    m = 2 #Pendiente de la linea
+    b = 3 #Punto de interseccion en el eje y
+    y = m*x + b # Formula para una funcion lineal
+
+    return y # Guardar la informacion de la varible y
+
+resultado = funcion_lineal(4) # Puede manipular el valor de 4 = x, osea x puede ser cualquier numero que usted quiere en este caso puse 4 como argumento, pero puede utilizar otros.
+print("El valor de 'y' + es: ", resultado)
